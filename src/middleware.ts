3c262c144068715682c9a8d1ba57fd634b84cfce
muhammadbilal10 <muhdbilal81@gmail.com>
2024-05-03 12:00:25 +0000

    Update middlerware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,27 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { db } from "./lib/db";
+import { getUserById } from "./actions/user";
 
 export default authMiddleware({
   publicRoutes: ["/"],
-  afterAuth(auth, req) {
+  afterAuth: async (auth, req) => {
     if (auth.userId && auth.isPublicRoute) {
+      // const existingUser = await db.user.findUnique({
+      //   where: {
+      //     id: auth.userId as string,
+      //   },
+      // });
+
+      // if (!existingUser) {
+      //   return NextResponse.redirect(new URL("/set-role", req.url));
+      // }
       const path = "/dashboard";
       const redirect = new URL(path, req.url);
       return NextResponse.redirect(redirect);
     }
     if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
   },
 });
